d018f0215fb04def53e95bd064bded28f0186ecc
Furkan Portakal <furkanportakalx@gmail.com>
2023-02-09 02:09:59 +0300

    add test for iddaa

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: "test-dash",
   e2e: {
-    baseUrl: "http://0.0.0.0:1234",
+    //baseUrl: "http://0.0.0.0:1234",
+    baseUrl: "https://www.iddaa.com",
   },
 });
